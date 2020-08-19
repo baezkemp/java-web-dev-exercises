db18d41b185f778f21589f720cf2cabc83c67cd8
Liza Baez Kemp <baezkemp@gmail.com>
2020-08-18 22:04:54 -0500

    Ch3 Exercise Create Files and Org

---
+++ src/exercises/ch3/ArrayListPractice.java
@@ -0,0 +1,13 @@
+package exercises.ch3;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+
+//    3.7.2.1 - Write a static method to find the sum of all the even numbers in an ArrayList. Within main, create a list with at least 10 integers and call your method on the list.
+//    3.7.2.2 - Write a static method to print out each word in a list that has exactly 5 letters.
+//    3.7.2.3 - Modify your code to prompt the user to enter the word length for the search.
+//    3.7.2.4 - BONUS: Instead of creating our own list of words, what if we want to use the string from the Array Practice section? Search “Java convert String to ArrayList” online to see how to split a string into the more flexible ArrayList collection.
+
+    }
+}
+
---
+++ src/exercises/ch3/HashMapPractice.java
@@ -0,0 +1,10 @@
+package exercises.ch3;
+
+public class HashMapPractice {
+    public static void main(String[] args) {
+
+//    3.7.3.1 - It takes in student names and ID numbers (as integers) instead of names and grades.
+//    3.7.3.2 - The keys should be the IDs and the values should be the names.
+//    3.7.3.3 - Modify the roster printing code accordingly.
+    }
+}
